0c7137443ba21a4aa29d347d78792f6c40e12b90
Joel Tiura <atomicfuzz@gmail.com>
2018-05-24 18:17:56 -0700

    general re-formatting and some more descriptive renaming

--- kuka_arm/scripts/IK_debug.py
+++ kuka_arm/scripts/IK_debug.py
@@ -19,79 +19,6 @@ from IK_server import handle_calculate_IK
 
 
 # single pose test cases
-    theta1, theta2, theta3, theta4, theta5, theta6 = angles
-
-    ########################################################################################
-    ## For additional debugging add your forward kinematics here. Use your previously calculated thetas
-    ## as the input and output the position of your end effector as your_ee = [x,y,z]
-
-    q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')  # theta angles
-
-    fk_wrist = Matrix([
-        [(1.25*sin(q2) - 0.054*sin(q2 + q3) + 1.5*cos(q2 + q3) + 0.35)*cos(q1)],
-        [(1.25*sin(q2) - 0.054*sin(q2 + q3) + 1.5*cos(q2 + q3) + 0.35)*sin(q1)],
-        [         -1.5*sin(q2 + q3) + 1.25*cos(q2) - 0.054*cos(q2 + q3) + 0.75]])
-    fk_wrist = fk_wrist.evalf(subs={q1: theta1, q2: theta2, q3: theta3})
-
-    # fk_EE is the translation vector from the total homogeneous transform
-    # between the base and the end effector; this representation has frame error
-    # correction baked in!
-    fk_EE = Matrix([
-        [-0.303*(sin(q1)*sin(q4) + sin(q2 + q3)*cos(q1)*cos(q4))*sin(q5) + (1.25*sin(q2) - 0.054*sin(q2 + q3) + 1.5*cos(q2 + q3) + 0.35)*cos(q1) + 0.303*cos(q1)*cos(q5)*cos(q2 + q3)],
-        [-0.303*(sin(q1)*sin(q2 + q3)*cos(q4) - sin(q4)*cos(q1))*sin(q5) + (1.25*sin(q2) - 0.054*sin(q2 + q3) + 1.5*cos(q2 + q3) + 0.35)*sin(q1) + 0.303*sin(q1)*cos(q5)*cos(q2 + q3)],
-        [-0.303*sin(q5)*cos(q4)*cos(q2 + q3) - 0.303*sin(q2 + q3)*cos(q5) - 1.5*sin(q2 + q3) + 1.25*cos(q2) - 0.054*cos(q2 + q3) + 0.75]])
-
-    end_effector = fk_EE.evalf(subs={q1: theta1, q2: theta2, q3: theta3, q4: theta4, q5: theta5, q6: theta6})
-
-    ## End your code input for forward kinematics here!
-    ########################################################################################
-
-    ## For error analysis please set the following variables of your WC location and EE location in the format of [x,y,z]
-    your_wc = list(fk_wrist) # <--- Load your calculated WC values in this array
-    # your_ee = [fk_EE[0], fk_EE[1], fk_EE[2]] # <--- Load your calculated end effector value from your forward kinematics
-    your_ee = list(end_effector)
-    ########################################################################################
-    # Find WC error
-    if not(sum(your_wc)==3):
-        wc_x_e = abs(your_wc[0]-test_case[1][0])
-        wc_y_e = abs(your_wc[1]-test_case[1][1])
-        wc_z_e = abs(your_wc[2]-test_case[1][2])
-        wc_offset = sqrt(wc_x_e**2 + wc_y_e**2 + wc_z_e**2)
-        print ("\nWrist error for x position is: %04.8f" % wc_x_e)
-        print ("Wrist error for y position is: %04.8f" % wc_y_e)
-        print ("Wrist error for z position is: %04.8f" % wc_z_e)
-        print ("Overall wrist offset is: %04.8f units" % wc_offset)
-
-    # Find theta errors
-    t_1_e = abs(theta1 - test_case[2][0])
-    t_2_e = abs(theta2 - test_case[2][1])
-    t_3_e = abs(theta3 - test_case[2][2])
-    t_4_e = abs(theta4 - test_case[2][3])
-    t_5_e = abs(theta5 - test_case[2][4])
-    t_6_e = abs(theta6 - test_case[2][5])
-    print ("\nTheta 1 error is: %04.8f" % t_1_e)
-    print ("Theta 2 error is: %04.8f" % t_2_e)
-    print ("Theta 3 error is: %04.8f" % t_3_e)
-    print ("Theta 4 error is: %04.8f" % t_4_e)
-    print ("Theta 5 error is: %04.8f" % t_5_e)
-    print ("Theta 6 error is: %04.8f" % t_6_e)
-    print "Theta 1 Calculated : %04.2f" % theta1, " Actual : ", test_case[2][0]
-    print "Theta 2 Calculated : %04.3f" % theta2, " Actual : ", test_case[2][1]
-    print "Theta 3 Calculated : %04.2f" % theta3, " Actual : ", test_case[2][2]
-    print "Theta 4 Calculated : %04.2f" % theta4, " Actual : ", test_case[2][3]
-    print "Theta 5 Calculated : %04.2f" % theta5, " Actual : ", test_case[2][4]
-    print "Theta 6 Calculated : %04.2f" % theta6, " Actual : ", test_case[2][5]
-
-    # Find FK EE error
-    if not(sum(your_ee)==3):
-        ee_x_e = abs(your_ee[0]-test_case[0][0][0])
-        ee_y_e = abs(your_ee[1]-test_case[0][0][1])
-        ee_z_e = abs(your_ee[2]-test_case[0][0][2])
-        ee_offset = sqrt(ee_x_e**2 + ee_y_e**2 + ee_z_e**2)
-        print ("\nEnd effector error for x position is: %04.8f" % ee_x_e)
-        print ("End effector error for y position is: %04.8f" % ee_y_e)
-        print ("End effector error for z position is: %04.8f" % ee_z_e)
-        print ("Overall end effector offset is: %04.8f units \n" % ee_offset)
 test_cases = {
     0: [
         [[2.1529, 0, 1.9465], [0.0, -0.0, 0.0, 1.0]], # Inputs ([EE position], [EE orientation as quaternions])
@@ -191,22 +118,92 @@ def debug_code(test_case):
 	
 	## Error analysis
 	print ("\nTotal run time to calculate joint angles from pose is %04.4f seconds" % (time()-start_time))
+	
+	theta1, theta2, theta3, theta4, theta5, theta6 = ik_angles
+ 
 	check_joint_limits(ik_angles)
+	########################################################################################
+	## Forward kinematics implementation to verify results:
+	
+	q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')  # theta angles
+	# pre-derived wrist center position vector in terms of q1, q2, and q3 for comparison with
+	fk_wrist = Matrix([
+		[(1.25*sin(q2) - 0.054*sin(q2 + q3) + 1.5*cos(q2 + q3) + 0.35)*cos(q1)],
+		[(1.25*sin(q2) - 0.054*sin(q2 + q3) + 1.5*cos(q2 + q3) + 0.35)*sin(q1)],
+		[         -1.5*sin(q2 + q3) + 1.25*cos(q2) - 0.054*cos(q2 + q3) + 0.75],
+    ])
+	   
+	fk_wrist = fk_wrist.evalf(subs={q1: theta1, q2: theta2, q3: theta3})
+	# fk_EE is the additive, translational component of the total homogeneous transform between the base
+    # and the end effector; this column vector representation has frame error correction baked in!
+	fk_EE = Matrix([
+		[-0.303*(sin(q1)*sin(q4) + sin(q2 + q3)*cos(q1)*cos(q4))*sin(q5) + (1.25*sin(q2) - 0.054*sin(q2 + q3) + 1.5*cos(q2 + q3) + 0.35)*cos(q1) + 0.303*cos(q1)*cos(q5)*cos(q2 + q3)],
+		[-0.303*(sin(q1)*sin(q2 + q3)*cos(q4) - sin(q4)*cos(q1))*sin(q5) + (1.25*sin(q2) - 0.054*sin(q2 + q3) + 1.5*cos(q2 + q3) + 0.35)*sin(q1) + 0.303*sin(q1)*cos(q5)*cos(q2 + q3)],
+		[-0.303*sin(q5)*cos(q4)*cos(q2 + q3) - 0.303*sin(q2 + q3)*cos(q5) - 1.5*sin(q2 + q3) + 1.25*cos(q2) - 0.054*cos(q2 + q3) + 0.75]])
+	fk_EE = fk_EE.evalf(subs={q1: theta1, q2: theta2, q3: theta3, q4: theta4, q5: theta5, q6: theta6})
+
+	########################################################################################
+	## For error analysis, the following names are given to the locations of the wrist center (wc) and
+	## end effector (ee) in the format of [x, y, z]
+	calculated_wc = list(fk_wrist) # <--- The calculated wc coordinates
+	# your_ee = [fk_EE[0], fk_EE[1], fk_EE[2]] # <--- Load your calculated end effector value from your forward kinematics
+	calculated_ee = list(fk_EE)
+	########################################################################################
+	
+	## Error analysis
+	print ("\nTotal run time to calculate joint angles from pose is %04.4f seconds" % (time()-start_time))
+	
+	# Find WC error
+	if not(sum(calculated_wc)==3):
+		wc_x_e = abs(calculated_wc[0] - test_case[1][0])
+		wc_y_e = abs(calculated_wc[1] - test_case[1][1])
+		wc_z_e = abs(calculated_wc[2] - test_case[1][2])
+		wc_offset = sqrt(wc_x_e**2 + wc_y_e**2 + wc_z_e**2)
+		print ("\nWrist error for x position is: %04.8f" % wc_x_e)
+		print ("Wrist error for y position is: %04.8f" % wc_y_e)
+		print ("Wrist error for z position is: %04.8f" % wc_z_e)
+		print ("Overall wrist offset is: %04.8f units" % wc_offset)
+	
+	# calculate theta errors
+	t_1_e = abs(theta1 - test_case[2][0])
+	t_2_e = abs(theta2 - test_case[2][1])
+	t_3_e = abs(theta3 - test_case[2][2])
+	t_4_e = abs(theta4 - test_case[2][3])
+	t_5_e = abs(theta5 - test_case[2][4])
+	t_6_e = abs(theta6 - test_case[2][5])
+	print ("\nTheta 1 error is: %04.8f" % t_1_e)
+	print ("Theta 2 error is: %04.8f" % t_2_e)
+	print ("Theta 3 error is: %04.8f" % t_3_e)
+	print ("Theta 4 error is: %04.8f" % t_4_e)
+	print ("Theta 5 error is: %04.8f" % t_5_e)
+	print ("Theta 6 error is: %04.8f" % t_6_e)
+	print ("Theta 1 Calculated : %04.2f" % theta1, " Actual : ", test_case[2][0])
+	print ("Theta 2 Calculated : %04.3f" % theta2, " Actual : ", test_case[2][1])
+	print ("Theta 3 Calculated : %04.2f" % theta3, " Actual : ", test_case[2][2])
+	print ("Theta 4 Calculated : %04.2f" % theta4, " Actual : ", test_case[2][3])
+	print ("Theta 5 Calculated : %04.2f" % theta5, " Actual : ", test_case[2][4])
+	print ("Theta 6 Calculated : %04.2f" % theta6, " Actual : ", test_case[2][5])
+
+	# Find FK EE error
+	if not(sum(calculated_ee) == 3):
+		x_err = abs(calculated_ee[0]-test_case[0][0][0])
+		y_err = abs(calculated_ee[1]-test_case[0][0][1])
+		z_err = abs(calculated_ee[2]-test_case[0][0][2])
+		ee_offset = sqrt(x_err**2 + y_err**2 + z_err**2)
+		print ("\nEnd effector error for x position is: %04.8f" % x_err)
+		print ("End effector error for y position is: %04.8f" % y_err)
+		print ("End effector error for z position is: %04.8f" % z_err)
+		print ("Overall end effector offset is: %04.8f units \n" % ee_offset)
+
+
 if __name__ == "__main__":
-    # print "\n\nTest initializing at time: {}".format(strftime('%H:%M:%S'))
-    print "\n"+(40 * '*v')+" BEGIN"
-    print "Test initializing at time: {}".format(strftime('%c'))
-    # Change test case number for different scenarios
-    for number in sorted(test_cases.keys()):
-        if test_cases[number]:
-            print "\n" + 40 * "- "+ str(number)
-            print "Running test case {}...".format(number)
-    print "\nTest completed at time: {}".format(strftime('%H:%M:%S'))
-    print 40 * '*^' + " END \n"
-
-# if __name__ == "__main__":
-#     # Change test case number for different scenarios
-#     test_case_number = 1
-
-#     test_code(test_cases[test_case_number])
+	print "\n" + (40 * '*v') + " BEGIN"
+	print "Test initializing at time: {}".format(strftime('%c'))
+	# Cycle through all provided debug requests
+	for number in sorted(test_cases.keys()):
+		if test_cases[number]:
+			print "\n" + 40 * "- " + str(number)
+			print "Running test case {}...".format(number)
 			debug_code(test_cases[number])
+	print "\nTest completed at time: {}".format(strftime('%H:%M:%S'))
+	print 40 * '*^' + " END \n"
